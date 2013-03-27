b062920731ae9917c3f478a914a786b574b28d07
Rami Al-Ghanmi <rami@ramialghanmi.com>
2013-03-27 10:21:07 -0700

    fix script to work POST requests

--- scripts/logger_post-receive-hook.py
+++ scripts/logger_post-receive-hook.py
@@ -1,17 +1,18 @@
 import cgi
 import cgitb
+import os
 import simplejson
 from datetime import datetime
 
 def get_commit_log(commit):
-    """ Given a commit dict object return a log string """
-    commit_sha = commit['id'] 
-    commit_author = '{} ({}) <{}>'.format(commit['author']['name'], commit['author']['username'], commit['author']['email'])
-    commit_timestamp = commit['timestamp']
-    commit_message = commit['message']
-    log_commit = '{}, {}, {}, "{}"'.format(commit_sha, commit_author, commit_timestamp, commit_message)
-    
-    return log_commit
+	""" Given a commit dict object return a log string """
+	commit_sha = commit['id'] 
+	commit_author = '{} ({}) <{}>'.format(commit['author']['name'], commit['author']['username'], commit['author']['email'])
+	commit_timestamp = commit['timestamp']
+	commit_message = commit['message']
+	log_commit = '{}, {}, {}, "{}"'.format(commit_sha, commit_author, commit_timestamp, commit_message)
+
+	return log_commit
 
 """ Print HTTP Response """
 print 'Content-Type: text/html'
@@ -32,44 +33,54 @@ args = cgi.FieldStorage()
 call_time = datetime.now().strftime('%Y-%m-%d %I:%M:%S%p %z')
 log_message = ''
 
+# Check if this is a POST request
+if os.environ['REQUEST_METHOD'] != 'POST':
+	log_message = '[ERROR {}] Invalid HTTP request. Only POST requests accepted'.format(call_time)
+
 # Check if payload exists
-if 'payload' not in args:
-    log_message = '[ERROR {}] Invalid request. Missing JSON payload'.format(call_time)
+#if 'payload' not in args:
+#    log_message = '[ERROR {}] Invalid request. Missing JSON payload'.format(call_time)
 
 else:
-    # Attempt to reason payload as JSON object and parse.
-    try:
-        """ Log Message Contents """
-        log_lead = '[LOG {}]'.format(call_time)
-        payload = simplejson.loads(args['payload'].value)
+	""" Obtain payload """
+	if getattr(args, 'file'):
+		try:
+			""" Log Message Contents """
+			log_lead = '[LOG {}]'.format(call_time)
+			#payload = simplejson.loads(args['payload'].value)
+			payload = simplejson.loads(args.file.read())
 
-        repo_org = payload['repository']['owner']['name']
-        repo_name = payload['repository']['name']
-        log_repo = '[{}/{}]'.format(repo_org, repo_name)
+			repo_org = payload['repository']['owner']['name']
+			repo_name = payload['repository']['name']
+			log_repo = '[{}/{}]'.format(repo_org, repo_name)
 
-        commit_count = len(payload['commits'])
-        
-        # Treat multi-commit pushes with more care
-        if commit_count > 1:
-            commit_latest = payload['head_commit']['id']
-            log_message = '{}{} Push with {} commits. Latest {}'.format(log_lead, log_repo, commit_count, commit_latest)
-            for c in payload['commits']:
-                single_commit = get_commit_log(c)
-                log_message += '\n{}{} {}'.format(log_lead, log_repo, single_commit)
+			commit_count = len(payload['commits'])
 
+			# Treat multi-commit pushes with more care
+			if commit_count > 1:
+				commit_latest = payload['head_commit']['id']
+				log_message = '{}{} Push with {} commits. Latest {}'.format(log_lead, log_repo, commit_count, commit_latest)
+				for c in payload['commits']:
+					single_commit = get_commit_log(c)
+					log_message += '\n{}{} {}'.format(log_lead, log_repo, single_commit)
+
+			else:
+				log_commit = get_commit_log(payload['head_commit'])
+				log_message = '{}{} {}'.format(log_lead, log_repo, log_commit)
+
+			""" Write Ouput to File """
+			logFileName = '{}.{}.log'.format(repo_org, repo_name)
+			logFile = open(logFileName, 'a')
+			logFile.writelines(log_message)
+			logFile.writelines('\n\n')
+			logFile.close()
+		except ValueError:
+			log_message = '[ERROR {}] Error Parsing JSON payload'.format(call_time)
         else:
-            log_commit = get_commit_log(payload['head_commit'])
-            log_message = '{}{} {}'.format(log_lead, log_repo, log_commit)
-
-        """ Write Ouput to File """
-        logFileName = '{}.{}.log'.format(repo_org, repo_name)
-        logFile = open(logFileName, 'a')
-        logFile.writelines(log_message)
-        logFile.writelines('\n\n')
-        logFile.close()
+            log_message = '[ERROR {}] Invalid POST request. No payload'.format(call_time)
+	
         
-    except ValueError:
-        log_message = '[ERROR {}] Error Parsing JSON payload'.format(call_time)
+
 
 
 """ Write Ouput to HTML """
